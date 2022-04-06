0b8eb25eaaaf1d4ab08b4d1ec029f2561b77166a
JoyTriza <wangarijoytriza@gmail.com>
2022-04-06 10:53:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
 }
+let input = document.getElementById('input');
+
+    function clickAlert(){
+        alert("");
+    }
+
+    input.addEventListener('click',clickAlert);
+
+document.addEventListener()
+
+
+
